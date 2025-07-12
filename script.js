6e275a581c5612de8e4317d7efced7a939b3ec8b
KIMyeonjae0514 <kyj20130514v@gmail.com>
2025-07-12 14:26:27 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+ function magic(event) {
+            alert("김연재 꽃미남 김연재 꽃미남 김연재 꽃미남 김연재 꽃미남김연재 꽃미남 김연재 꽃미남김연재 꽃미남김연재 꽃미남김연재 꽃미남김연재 꽃미남")
+            document.body.style.backgroundColor = "blue"
+        }  
